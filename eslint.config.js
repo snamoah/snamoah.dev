79be43ca4ed5626cee01cf5bc96d790cc25592f2
Samuel Amoah <sa.am@programmer.net>
2021-07-21 20:28:01 +0200

    build(): configure husky

---
+++ eslint.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    extends: [
+        'react-app',
+        "plugin:jsx-a11y/recommended",
+        "plugin:prettier/recommended",
+        "prettier/react"
+    ],
+    plugins: ['jsx-a11y']
+}
