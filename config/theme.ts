909f63c6efd4b4791273320aab2cf726e16a5979
Samuel Amoah <sa.am@programmer.net>
2021-07-28 02:54:01 +0200

    feat(): add new theme colors

--- config/theme.ts
+++ config/theme.ts
@@ -1,9 +1,13 @@
 enum colors {
   PRIMARY = '#7366FF',
   BLACK = '#1F2126',
-  GREY = '#EBEDF2',
-  LIGHT_GREY = '#C4C4C4',
-  DARK_GREY = '#4C5059',
+
+  /* Grey */
+  LIGHTER_GREY = '#F2F4F7',
+  LIGHT_GREY = '#EBEDF2',
+  GREY = '#C4C4C4',
+  DARK_GREY = '#959BA6',
+  DARKER_GREY = '#4C5059',
 }
 
 const theme = {
